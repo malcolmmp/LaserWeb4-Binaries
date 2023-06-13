5612aaddaf48b74cc17b8de412414852ac303d15
Anders Rottschafer <github@anders.place>
2023-06-13 15:50:10 -0400

    got past a few errors for the mac build script

--- build.osx.sh
+++ build.osx.sh
@@ -26,10 +26,12 @@ fi
 git clone https://github.com/Rottschaferanders/LaserWeb4.git $LW_DIR
 cd $LW_DIR
 git checkout $TARGET_UI_BRANCH
+nvm install v10.16.3
 yarn
 npm run installdev
 
-export UI_VERSION=$(git describe --abbrev=0 --tags)
+# export UI_VERSION=$(git describe --abbrev=0 --tags)
+export UI_VERSION="1.0.0"
 export SERVER_VERSION=$(cat ./node_modules/lw.comm-server/version.txt | cut -c 3-6)
 
 # Bundle LaserWeb app using webpack
@@ -49,5 +51,5 @@ echo "BUILDING Laserweb $UI_VERSION-$SERVER_VERSION"
 
 # Copy web front-end + build server component
 ./node_modules/.bin/electron-rebuild
-./node_modules/.bin/build --em.version=$UI_VERSION-$SERVER_VERSION -p never
-#./node_modules/.bin/build --linux --em.version=$UI_VERSION-$SERVER_VERSION -p never
+# ./node_modules/.bin/build --em.version=$UI_VERSION-$SERVER_VERSION -p never
+./node_modules/.bin/build --linux --em.version=$UI_VERSION-$SERVER_VERSION -p never
